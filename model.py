c59037f4b8b81682933fee374d1479d06b3a3ca4
Gabriel <gschull991@gmail.com>
2024-03-04 21:49:13 -0500

    Multi head attention layer added

--- model.py
+++ model.py
@@ -53,6 +53,64 @@ class LayerNormalization(nn.Module):
 
     def forward(self, x):
         mean = x.mean(dim = -1, keepdim = True)
-        std = x.std(dim = -1, keepdim =True)
+        std = x.std(dim = -1, keepdim = True)
         return self.gamma * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForwarBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout) # W1 & B1
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.linear_2 = nn.Linear(d_ff, d_model)  # W2 & B2
+
+    def forward(self, x):
+        # (Batch, max_len, d_model) --> (Batch, max_len, d_ff) --> (Batch, max_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model must be divisible by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+        self.w_o = nn.Linear(d_model, d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        
+        # (Batch, h , max_len, d_k) --> (Batch, h, max_len, max_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_ (mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) # (Batch, h, max_len, max_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return (attention_scores @ value), attention_scores
+
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, max_len, d_model) --> (Batch, max_len, d_model)
+        key = self.w_k(k) # (Batch, max_len, d_model) --> (Batch, max_len, d_model)
+        value = self.w_v(v) # (Batch, max_len, d_model) --> (Batch, max_len, d_model)
+
+        # Split the d_model into h heads and transpose the result; (Batch, max_len, d_model) --> (Batch, max_len, h, d_k) --> (Batch, h, max_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, self.dropout)
+
+        # (Batch, h, max_len, d_k) --> (Batch, max_len, h, d_k) --> (Batch, max_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h* self.d_k)
+
+        # (Batch, max_len, d_model) --> (Batch, max_len, d_model)
+        return self.w_o(x)
